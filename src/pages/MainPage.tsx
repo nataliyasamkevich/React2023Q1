cd0600cdfb3ec582e16979d84b129148eb17c49d
Nataliya Samkevich <nataliyasamkevich@gmail.com>
2023-03-17 09:08:38 +0100

    feat: add search bar

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SearchBar from './../components/searchBar';
 import CardsWrapper from './../components/cardsWrapper';
 
 function MainPage() {
   return (
     <main>
+      <SearchBar />
       <CardsWrapper />
     </main>
   );
