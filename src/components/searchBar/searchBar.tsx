7d26159bf18ed4957498a02d4021b9322c989499
Nataliya Samkevich <nataliyasamkevich@gmail.com>
2023-04-02 18:02:12 +0200

    refactor: change searchBar from class to function component

--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,39 +1,48 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './searchBar.css';
 
-class SearchBar extends React.Component<{ text?: string }, { value: string }> {
-  constructor(props: { text?: string }) {
-    super(props);
-    this.state = { value: localStorage.getItem('searchBarValue') || '' };
-  }
+function SearchBar() {
+  const [searchBarValue, setSearchBarValue] = useState(
+    localStorage.getItem('searchBarValue') ?? ''
+  );
+  const searchRef = useRef('');
 
-  componentWillUnmount = () => {
-    localStorage.setItem('searchBarValue', this.state.value);
+  useEffect(() => {
+    return () => {
+      localStorage.setItem('searchBarValue', searchRef.current || '');
+    };
+  }, []);
+
+  useEffect(() => {
+    searchRef.current = searchBarValue;
+  }, [searchBarValue]);
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    setSearchBarValue(event.target.value);
   };
 
-  handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    this.setState({ value: event.target.value });
+  const handleClearClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+    setSearchBarValue('');
   };
 
-  handleClearClick: React.MouseEventHandler<HTMLButtonElement> = () => {
-    this.setState({ value: '' });
+  const handleFormSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
   };
 
-  render() {
-    return (
-      <div className="search">
-        <input
-          className="input search__input"
-          type="search"
-          value={this.state.value}
-          placeholder="Search for..."
-          onChange={this.handleChange}
-        />
-        <button className="button search-button"></button>
-        <button className="button clear-button" onClick={this.handleClearClick}></button>
-      </div>
-    );
-  }
+  return (
+    <form className="search" onSubmit={handleFormSubmit}>
+      <input
+        className="input search__input"
+        type="search"
+        value={searchBarValue}
+        placeholder="Search for..."
+        onChange={handleChange}
+      />
+      <button className="button search-button"></button>
+      <button className="button clear-button" onClick={handleClearClick}></button>
+    </form>
+  );
 }
 
 export default SearchBar;
